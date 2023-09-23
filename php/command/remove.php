cb630c824c8229637fdc5973aaebf4d53b028519
Alexander Mikhno <57302726+mixno35@users.noreply.github.com>
2023-09-23 13:58:54 +0300

    Fix remove directories

--- php/command/remove.php
+++ php/command/remove.php
@@ -3,18 +3,33 @@ global $data;
 
 $path = $data["path"] ?? "";
 
-if (is_dir($path)) {
-    if (rmdir($path)) {
-        echo json_encode([
-            "type" => "success",
-            "message_id" => "api_remove_dir_success"
-        ], 128);
-    } else {
-        echo json_encode([
-            "type" => "error",
-            "message_id" => "api_remove_dir_error"
-        ], 128);
+function delete_directory($dirPath):void {
+    if (is_dir($dirPath)) {
+        $files = scandir($dirPath);
+        foreach ($files as $file) {
+            if ($file != "." && $file != "..") {
+                $filePath = $dirPath . DIRECTORY_SEPARATOR . $file;
+                if (is_dir($filePath)) {
+                    // Рекурсивно удалить подпапку
+                    delete_directory($filePath);
+                } else {
+                    // Удалить файл
+                    unlink($filePath);
+                }
+            }
+        }
+        // Удалить пустую папку
+        rmdir($dirPath);
     }
+}
+
+if (is_dir($path)) {
+    delete_directory($path);
+
+    echo json_encode([
+        "type" => "success",
+        "message_id" => "api_remove_dir_success"
+    ], 128);
 
     exit();
 }
