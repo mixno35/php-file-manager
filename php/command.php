2a51c29421dae3762a9afbc541dd3d4af41498f1
Alexander Mikhno <57302726+mixno35@users.noreply.github.com>
2023-09-23 21:04:25 +0300

    Added new commands

--- php/command.php
+++ php/command.php
@@ -6,9 +6,18 @@ $command = $data["command"] ?? ""; // Извлекаем название ком
 if (str_starts_with("remove", $command)) { // Удаление файла/папки
     include_once "command/remove.php";
     exit();
+} if (str_starts_with("rename", $command)) { // Переименование файла/папки
+    include_once "command/rename.php";
+    exit();
+} if (str_starts_with("create-dir", $command)) { // Создание папки
+    include_once "command/create-dir.php";
+    exit();
+} if (str_starts_with("create-file", $command)) { // Создание файла
+    include_once "command/create-file.php";
+    exit();
 }
 
 echo json_encode([
     "type" => "error",
-    "message_id" => "api_unknown_command"
+    "message_id" => "api_command_unknown"
 ], 128);
