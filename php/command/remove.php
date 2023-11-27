13b24ff09e62d16c610fde21683bdb767d0c242f
Alexander Mikhno <57302726+mixno35@users.noreply.github.com>
2023-11-28 01:12:12 +0300

    Update remove.php

--- php/command/remove.php
+++ php/command/remove.php
@@ -3,36 +3,29 @@ global $data, $path_manager;
 
 $paths = explode(", ", $data["path"]) ?? [];
 
-//die(print_r($path, true));
+$removed_count = 0;
 
 function delete_directory($dirPath):void {
+    global $removed_count;
     if (is_dir($dirPath)) {
         $files = scandir($dirPath);
         foreach ($files as $file) {
             if ($file != "." && $file != "..") {
                 $filePath = $dirPath . DIRECTORY_SEPARATOR . $file;
-                if (is_dir($filePath)) {
-                    // Рекурсивно удалить подпапку
-                    delete_directory($filePath);
-                } else {
-                    // Удалить файл
-                    unlink($filePath);
-                }
+                if (is_dir($filePath)) delete_directory($filePath);
+                else unlink($filePath);
             }
         }
-        // Удалить пустую папку
-        rmdir($dirPath);
+
+        if (rmdir($dirPath)) $removed_count++;
     }
 }
-
-$removed_count = 0;
 
 foreach ($paths as $path) {
     if (is_readable($path))
         if ($path_manager->chmod_detect($path))
             if (is_dir($path)) {
                 delete_directory($path);
-                $removed_count++;
             } else if (is_file($path) && file_exists($path)) {
                 if (unlink($path)) $removed_count++;
             }
