7c41f3cf6c5ef0b0f611040d8b7ad4f3d4bbcbeb
Alexander Mikhno <57302726+mixno35@users.noreply.github.com>
2023-12-10 07:24:07 +0300

    Reading a file in chunks and sending to output

--- content/blob.php
+++ content/blob.php
@@ -20,45 +20,60 @@ if (!$privileges["view_file"]) {
     exit();
 }
 
+include_once dirname(__FILE__, 2) . "/class/URLParse.php";
+
+$url_parse = new URLParse();
+
 $blob_path = $_GET["p"] ?? "";
 
-if (!file_exists($blob_path)) {
-    http_response_code(404);
-    exit();
-}
-
-$last_modified_time = filemtime($blob_path);
-$etag = md5_file($blob_path);
-
-if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) || isset($_SERVER["HTTP_IF_NONE_MATCH"])) {
-    if (strtotime($_SERVER["HTTP_IF_MODIFIED_SINCE"]) >= $last_modified_time || trim($_SERVER["HTTP_IF_NONE_MATCH"], "\"") === $etag) {
-        http_response_code(304);
+if ($url_parse->is_blob_url($blob_path)) {
+    header("Refresh: 0; URL=$blob_path");
+} else {
+    if (!file_exists($blob_path)) {
+        http_response_code(404);
         exit();
     }
+
+    $last_modified_time = filemtime($blob_path);
+    $etag = md5_file($blob_path);
+
+    if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) || isset($_SERVER["HTTP_IF_NONE_MATCH"])) {
+        if (strtotime($_SERVER["HTTP_IF_MODIFIED_SINCE"]) >= $last_modified_time || trim($_SERVER["HTTP_IF_NONE_MATCH"], "\"") === $etag) {
+            http_response_code(304);
+            exit();
+        }
+    }
+
+    $blob_mime_type = mime_content_type($blob_path);
+
+    if ($blob_mime_type === false) {
+        http_response_code(500);
+        exit();
+    }
+
+    $filename = basename($blob_path);
+
+    $handle = fopen($blob_path, "rb");
+
+    if ($handle === false) {
+        http_response_code(500);
+        exit();
+    }
+
+    header("Content-Type: $blob_mime_type");
+    header("Content-Length: " . filesize($blob_path));
+    header("Content-Disposition: inline; filename=\"$filename\"");
+    header("Cache-Control: public, max-age=0");
+    header("Pragma: no-cache");
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s", $last_modified_time) . " GMT");
+    header("Etag: $etag");
+
+    while (!feof($handle)) {
+        $chunk = fread($handle, 8192);
+        echo $chunk;
+        ob_flush();
+        flush();
+    }
+
+    fclose($handle);
 }
-
-$blob_mime_type = mime_content_type($blob_path);
-
-if ($blob_mime_type === false) {
-    http_response_code(500);
-    exit();
-}
-
-// Получаем имя файла без пути
-$filename = basename($blob_path);
-
-// Отправляем правильные заголовки
-header("Content-Type: $blob_mime_type");
-header("Content-Length: " . filesize($blob_path));
-header("Content-Disposition: inline; filename=\"$filename\"");
-header("Cache-Control: public, max-age=0");
-header("Pragma: no-cache");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s", $last_modified_time) . " GMT");
-header("Etag: $etag");
-
-if (!readfile($blob_path)) {
-    http_response_code(500);
-    exit();
-}
-
-readfile($blob_path);
